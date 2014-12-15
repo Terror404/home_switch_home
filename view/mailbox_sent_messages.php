e842eddef8242e5360cf58e5d99eb41c72c72695
MelJoyeuse <melanie.mathey@isep.fr>
2014-12-15 16:58:42 +0100

    Merge origin/master
    Conflicts:
    	Pierre/HouseCard/create_hc_modele.php
    	modele/add_house.php
    	modele/search_ad.php
    	modele/search_profile_reminder.php
    	view/create_house.php
    	view/css/hsh_style.css
    	view/my_profile.php
    	view/profile_reminder.php

---
+++ view/mailbox_sent_messages.php
@@ -0,0 +1,33 @@
+
+<?php
+include('mailbox_toolbar.php');
+?>
+
+<?php
+require("../modele/search_my_mp.php");
+?>
+
+<table>
+                <caption> messages envoyés </caption>
+                        <tr>
+                                <th> envoyé à </th>
+                                <th> titre</th>
+                                <th> date d'envoi</th>
+
+                        </tr>
+    <?php       /* récupération et affichage des données*/
+    while ($res_sent_msg=$ask_sent_msg -> fetch ())
+        {?>
+                        <tr>
+                                 <td> <?php echo $res_sent_msg['id_receiver'] ?> </td>
+                                <td> <!-- liens vers la page de lecture de message , et envoi son id pour pouvoir lire son contenu -->
+                                    <a href="content.php?page=readMsg&id=<?php echo $res_sent_msg['id'] ?>">
+                                         <?php echo $res_sent_msg['title'] ?>
+                                    </a> 
+                                </td>
+                                <td> <?php echo $res_sent_msg ['date'] ?></td>
+                        </tr>
+    <?php 
+        }
+?>
+</table>
