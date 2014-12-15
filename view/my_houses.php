cf5a344b4ed0a3e393567ea416110bbbd1230a95
Pierre <pierre.prunieres@live.fr>
2014-12-15 19:32:33 +0100

    Merge origin/master
    Conflicts:
    	view/create_house.php

--- view/my_houses.php
+++ view/my_houses.php
@@ -1,8 +1,8 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+while($resHouse=$askHouse->fetch())
+{
+    ?>
+        titre n°<?php echo$resHouse['id']?>:<?php echo $resHouse['title'];
+}
+?>
