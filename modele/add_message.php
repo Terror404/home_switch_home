7362c27ed657514a36d79c24b8d642a826ad85e0
MelJoyeuse <melanie.mathey@isep.fr>
2014-12-15 17:43:45 +0100

    Merge origin/master
    Conflicts:
    	Pierre/HouseCard/create_hc_modele.php
    	modele/add_house.php
    	modele/search_ad.php
    	modele/search_profile_reminder.php
    	view/create_house.php
    	view/css/hsh_style.css
    	view/my_profile.php
    	view/profile_reminder.php

--- modele/add_message.php
+++ modele/add_message.php
@@ -5,16 +5,12 @@ session_start();
 <?php /* déclaration des variables*/
     $DB= new PDO ('mysql:host=localhost; dbname=home_switch_home', 'root','');
     $idAuthor = $_SESSION['userId'];
-    $logReceiver = $_POST ["login_receiver"];
+    $idReceiver = $_POST ["login_receiver"];
     $title  =$_POST ["title"];
     $text = $_POST ["text"];
 
-   /* transforme le login du receiver en son id dans la base de données */
-    $askIdRec = $DB -> prepare ('SELECT id FROM user WHERE login =: loginRec');
-    $askIdRec-> execute (array('loginRec' => $logReceiver));
-    $receiverData = $askIdRec->fetch();
-    
-    /* ajout du nouveau message à la base de données*/
+ 
+   
     $req = $DB -> prepare (
             'INSERT INTO messages(date, id_author, id_receiver, title, text)'
             . 'VALUES ( :date, :id_author, :id_receiver, :title, :text) '
@@ -22,7 +18,7 @@ session_start();
     $req->execute(array(
         'date' => date('d.m.y h:i:s'),
         'id_author' => $idAuthor,
-        'id_receiver' => $receiverData['id'],
+        'id_receiver' => $idReceiver,
         'title' => $title,
         'text' => $text,
        ));
