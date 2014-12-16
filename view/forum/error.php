7b646bc443977ee7d3426bdcd77366b8bfd8d30e
ThomAtchoum <thomas.augendre@isep.fr>
2014-12-16 13:20:52 +0100

    Plus de style!

--- view/forum/error.php
+++ view/forum/error.php
@@ -1,12 +1,16 @@
-Une erreur s'est produite :<br/>
-<?php
-    //$error should be set before including this page
-    if (isset($error)) {
-        echo $error;
-    }
-    else {
-        echo "Erreur inconnue.";
-    }
-?>
-<br/><br/>
-<a href="./../controler/content.php?page=forumIndex">Retour à l'index du forum</a>
+<link rel="stylesheet" type="text/css" href="./../view/css/forumstyle.css">
+
+<div class="forumFrame">
+    Une erreur s'est produite :<br/>
+    <?php
+        //$error should be set before including this page
+        if (isset($error)) {
+            echo $error;
+        }
+        else {
+            echo "Erreur inconnue.";
+        }
+    ?>
+    <br/><br/>
+    <a href="./../controler/content.php?page=forumIndex">Retour à l'index du forum</a>
+</div>
--- view/forum/newtopic_view.php
+++ view/forum/newtopic_view.php
@@ -1,10 +1,13 @@
 <script src="http://js.nicedit.com/nicEdit-latest.js" type="text/javascript"></script>
 <script type="text/javascript">bkLib.onDomLoaded(nicEditors.allTextAreas);</script>
+<link rel="stylesheet" type="text/css" href="./../view/css/forumstyle.css">
 
-<form method="post" action="./../controler/content.php?page=showForum&amp;c=<?php echo CURRENT_CATEGORY ?>">
-	Entrez le nom de votre sujet :<br/>
-	<input type="text" name="topicTitle"><br/>
-	Entrez le texte de votre message :<br/>
-	<textarea name="openingPost" rows="8" cols="50"></textarea><br/>
-	<input type="submit" value="Envoyer">
-</form>
+<div class="forumFrame">
+    <form method="post" action="./../controler/content.php?page=showForum&amp;c=<?php echo CURRENT_CATEGORY ?>">
+        Entrez le nom de votre sujet :<br/>
+        <input type="text" name="topicTitle"><br/>
+        Entrez le texte de votre message :<br/>
+        <textarea name="openingPost" rows="8" cols="50"></textarea><br/>
+        <input type="submit" value="Envoyer">
+    </form>
+</div>
