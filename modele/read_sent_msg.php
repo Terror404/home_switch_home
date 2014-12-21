d81ac6beae4de70b504f6ba1dd5b93f351e4fc3f
MelJoyeuse <melanie.mathey@isep.fr>
2014-12-21 17:02:26 +0100

    Merge origin/master
    Conflicts:
    	view/create_house.php

--- modele/read_sent_msg.php
+++ modele/read_sent_msg.php
@@ -1,14 +1,3 @@
-<?php   /*Accès à la bdd*/
-            try 
-                {
-                    $DB = new PDO ("mysql:host=localhost;dbname=home_switch_home","root","",array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-                } 
-            catch (Exception $ex) 
-                {
-                    die ('Erreur:'.$ex ->getMessage());
-                }
-?>
-
 <?php   /* récupère le message reçu sélectionné*/
     $askSentMsg=$DB->prepare("SELECT id, id_receiver, title, date,text FROM messages WHERE id_author=:userId AND id=:id_msg");
         $askSentMsg->execute(array('userId'=>$_SESSION['userId'],
