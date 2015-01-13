6984bdf501d519df73cc5f8b08d89ef1b73f6d75
MelJoyeuse <melanie.mathey@isep.fr>
2015-01-13 16:31:30 +0100

    Merge origin/master
    Conflicts:
    	controler/content.php
    	controler/secondary_controler.php
    	view/confirm_add_user.php
    	view/my_houses.php

--- view/confirm_add_user.php
+++ view/confirm_add_user.php
@@ -3,7 +3,7 @@
 <?php
 
 echo $_SESSION['profCree']
-?>]
+?>
     <br/>
     <input type="button" onclick="self.location.href='../controler/content.php?page=research'" name="continuer" value="Continuer" />
         </div>
--- view/form_user.php
+++ view/form_user.php
@@ -25,41 +25,41 @@
 </script>
 
             <div class="position">
-                <h1 class="Title"> <?php echo $_SESSION['$creCompt']; ?> </h1> </br>
+                <h1 class="Title"> <?php echo $_SESSION['creCompt']; ?> </h1> </br>
 		<form method="post" action="content.php?page=confirmAddUser" onsubmit="return validation(this)" class="form">
 			<p>
-                            <label for="pseudo" > <?php echo $_SESSION['$pseudo']; ?> </label>
+                            <label for="pseudo" > <?php echo $_SESSION['pseudo']; ?> </label>
 				<input type="text" name="pseudo" id="pseudo" placeholder="Ex:Doge92" maxlenght="12" />
 			</p>
                         
 			<p>
-				<label for="pass"> <?php echo $_SESSION['$pass']; ?> </label>
+				<label for="pass"> <?php echo $_SESSION['pass']; ?> </label>
 				<input type="password" name="pass" id="pass" />
 			</p>
 		
 		
 		
 			<p>
-				<label for="repass"> <?php echo $_SESSION['$repetpass']; ?> </label>
+				<label for="repass"> <?php echo $_SESSION['repetpass']; ?> </label>
 				<input type="password" name="repass" id="repass" />
 			</p>
 		
 		
 		
 			<p>
-				<label> <?php echo $_SESSION['$datNaiss']; ?> </label>  <input type="date" name="date" id="DatedeNaissance" placeholder="JJ/MM/AAAA" />
+				<label> <?php echo $_SESSION['datNaiss']; ?> </label>  <input type="date" name="date" id="DatedeNaissance" placeholder="JJ/MM/AAAA" />
 			</p>
 		
 
 		
 			<p>
-				<label> <?php echo $_SESSION['$adrMail']; ?> </label>  <input type="email" name="mail" id="Adressemail" placeholder="Ex:wowsuchadress@gmail.com" />
+				<label> <?php echo $_SESSION['adrMail']; ?> </label>  <input type="email" name="mail" id="Adressemail" placeholder="Ex:wowsuchadress@gmail.com" />
 			</p>
 		
 
 		
 			<p>
-				<label for="telephone"> <?php echo $_SESSION['$tel']; ?> </label>
+				<label for="telephone"> <?php echo $_SESSION['tel']; ?> </label>
 				<input type="tel" name="tel" id="tel" placeholder="Ex:0000000000" maxlength="10" />
 			</p>
                         <p><input type="hidden" mame="idmsg" value="2"></p>
@@ -84,7 +84,7 @@
     {
 ?>
         <div class="MsgErrorInsc">
-        <br/><br/> <?php echo $_SESSION['$pasAccedPag']; ?> <br/>
+        <br/><br/> <?php echo $_SESSION['pasAccedPag']; ?> <br/>
         <input type="button" value="Retour" onclick="self.location.href='content.php?page=home'" class="homeButton"/>
         </div>
     <?php }
