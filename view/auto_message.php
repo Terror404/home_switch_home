dc948723c344455b50f5e43b2ef7de692191e97c
hvalet <hvalet@pc-l312-6.isep.fr>
2015-01-15 09:03:03 +0100

    Merge origin/master
    Conflicts:
    	controler/secondary_controler.php
    	view/confirm_add_user.php

--- view/auto_message.php
+++ view/auto_message.php
@@ -6,7 +6,7 @@ $login=$_POST['pseudo'];
 $destinataire=$mail;
 $sujet="Activer votre compte";
 $entete="From : Home Switch Home";
-$message="Bonjour $login . Bienvenue sur Home Switch Home. Veuillez confirmer l'activation de votre compte.";
+$message="Bonjour $login . Bienvenue sur Home Switch Home. Vous pouvez maintenant échanger votre maison pour les vacances en quelques cliques.";
 
 mail($destinataire,$sujet,$message,$entete);
 
