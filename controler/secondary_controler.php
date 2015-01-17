a46b595337ac9dd471dfe6288f0ba173f462649c
Doge <clement.kubec@isep.fr>
2015-01-17 19:37:59 +0100

    Merge origin/master into HEAD
    Conflicts:
    	view/house_card.php
    	view/nav.php

--- controler/secondary_controler.php
+++ controler/secondary_controler.php
@@ -39,10 +39,7 @@ if (isset($page) AND $page!='')
                     {
                        echo'<link rel="stylesheet" type="text/css" href="../view/css/myhouses_style.css">'; 
                     }
-                    elseif( $_GET['page']== 'my_ads')
-                    {
-                        
-                    }
+                    
                     elseif( $_GET['page']=='my_mp')
                     {
                         echo'<link rel="stylesheet" type="text/css" href="../view/css/mailbox_style.css">';
