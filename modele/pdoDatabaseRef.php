00cf29f9b34bd3fbfdf939600f78d6401b9a038e
Doge <clement.kubec@isep.fr>
2015-01-19 00:58:01 +0100

    Merge origin/master
    Conflicts:
    	controler/ce qui reste a faire.php

--- modele/pdoDatabaseRef.php
+++ modele/pdoDatabaseRef.php
@@ -2,7 +2,7 @@
 
 try
 {
-	$DB = new PDO('mysql:host=localhost;dbname=home_switch_home', 'HsH', 'appG1A1AWebdesign', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	$DB = new PDO('mysql:host=localhost;dbname=home_switch_home', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
         $DB->query('set names utf8');
 }
 catch (exception $e)
